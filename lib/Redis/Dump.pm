b038a14e0978658b20596f71102df47c16592d00
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2011-06-14 23:34:56 -0300

    first commit

---
+++ lib/Redis/Dump.pm
@@ -0,0 +1,47 @@
+
+
+package Redis::Dump;
+
+use Moose;
+with 'MooseX::Getopt';
+
+use Redis;
+
+has server => (
+    is => 'rw',
+    isa => 'Str',
+    default => '127.0.0.1:6379'
+);
+
+has conn => (
+    is => 'rw',
+    isa => 'Redis',
+    lazy => 1,
+    default => sub { Redis->new( server => shift->server ) }
+);
+
+sub _get_keys {
+    shift->conn->keys("*");
+}
+
+sub _get_values_by_keys {
+    my $self = shift;
+    my %keys;
+    foreach my $key ($self->_get_keys) {
+        my $type = $self->conn->type($key);
+        $keys{$key} = $self->conn->get($key) if $type eq 'string';
+    }
+    return %keys;
+}
+
+sub run {
+    my $self = shift;
+
+    my %items;
+    $items{keys} = { $self->_get_values_by_keys };
+    
+    return %items;
+}
+
+1;
+
---
+++ t/keys.t
@@ -0,0 +1,28 @@
+#!perl
+
+use warnings;
+use strict;
+use Test::More;
+use Test::Exception;
+use Test::Deep;
+use IO::String;
+use Redis;
+use Redis::Dump;
+
+use lib 't/tlib';
+use Test::SpawnRedisServer;
+
+my ( $c, $srv ) = redis();
+END { $c->() if $c }
+
+ok( my $r = Redis->new( server => $srv ),
+    'connected to our test redis-server' );
+ok( my $dump = Redis::Dump->new( server => $srv ), 'run redis-dump' );
+
+$r->set( foo => 1 );
+$r->set( 'bar-test' => 2 );
+
+is_deeply( [ $dump->_get_keys ], [ 'foo', 'bar-test' ] );
+is_deeply( { $dump->run }, { keys => { 'bar-test' => '2', 'foo' => '1' } } );
+
+done_testing();
