9bbea3000290f72d4e8df24d956738ac486e6e62
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2011-06-15 00:06:32 -0300

    Support for hash.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- lib/Redis/Dump.pm
+++ lib/Redis/Dump.pm
@@ -31,6 +31,15 @@ sub _get_values_by_keys {
         my $type = $self->conn->type($key);
         $keys{$key} = $self->conn->get($key) if $type eq 'string';
         $keys{$key} = $self->conn->lrange($key, 0, -1) if $type eq 'list';
+        
+        if ($type eq 'hash') {
+            my %hash;
+            my @hashs = $self->conn->hkeys($key);
+            foreach my $item (@hashs) {
+                $hash{$item} = $self->conn->hget($key, $item);
+            }
+            $keys{$key} = { %hash } ;
+        }
     }
     return %keys;
 }
